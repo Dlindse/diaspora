32b912d8e464e447e0e49bb0681288b968f301d1
David Lindsey <david.a.lindsey@gmail.com>
2016-12-02 11:31:19 -0500

    migrated db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :references
+  resources :works
+  resources :genres
+  resources :authors
+  get 'pages/home'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
